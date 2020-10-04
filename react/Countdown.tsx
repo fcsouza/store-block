94da5195a95553ebc47460f99001956c9a872b0a
fcsouza <fcsouza18@hotmail.com>
2020-10-04 17:42:00 -0300

    creating countdown

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { TimeSplit } from './typings/global'
+import { tick, getTwoDaysFromNow } from './utils/time'
 
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
 interface CountdownProps {
   targetDate: string
 }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate }) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  })
+
   return (
     <div>
-      <h1>{ targetDate }</h1>
+      <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
     </div>
   )
 }
