4adae0c217a3c73ec1ef36aa14c5af5fc43c20bd
fcsouza <fcsouza18@hotmail.com>
2020-10-06 21:53:30 -0300

    include new block

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { useCssHandles } from 'vtex.css-handles'
 import { TimeSplit } from './typings/global'
 import { tick, getTwoDaysFromNow } from './utils/time'
-import { FormattedMessage } from 'react-intl'
 
 const titleText = title || <FormattedMessage id="countdown.title" />
 const DEFAULT_TARGET_DATE = getTwoDaysFromNow();
-const CSS_HANDLES = ["container", "countdown", "title"]
+const CSS_HANDLES = ["countdown"]
 interface CountdownProps {
-  title: string,
   targetDate: string
 }
 
@@ -19,17 +17,13 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = D
     seconds: '00'
   })
 
-  const titleText = title || <FormattedMessage id="countdown.title" />
   const handles = useCssHandles(CSS_HANDLES)
 
   tick(targetDate, setTime)
 
   return (
-    <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
-      <div className={`${handles.title} db tc`}>{titleText}</div>
-      <div className={`${handles.countdown} db tc`}>
-        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
-      </div>
+    <div className={`${handles.countdown} db tc`}>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
     </div>
   )
 }
@@ -39,11 +33,6 @@ Countdown.schema = {
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
-    title: {
-      title: 'Sou um título',
-      type: 'string',
-      default: null,
-    },
     targetDate: {
       title: 'Data final',
       description: 'Data final utilizada no contador',
